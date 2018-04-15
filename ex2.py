ceabf00ecd4b12241d108c45480820b68764b79d
Matanel <User@USER-THINK>
2018-04-15 21:47:57 +0300

    Ex2 is Done!

--- ex2.py
+++ ex2.py
@@ -5,7 +5,7 @@ from scipy.stats import norm
 
 # Create a normal distribution with the given parameters.
 def create_normal_distribution(mean, std):
-    normal_distribution = np.random.normal(mean, std, 1000)  # Create the normal distribution.
+    normal_distribution = np.random.normal(mean, std, 100)  # Create the normal distribution.
     np.random.shuffle(normal_distribution)  # Shuffle the data.
     return normal_distribution
 
@@ -18,13 +18,12 @@ def create_tag_list(points_list, tag):
 
 # The softMax function.
 def soft_max(w):
-    #e = np.exp(w)
     e = np.exp(w - np.max(w))
     dist = e / e.sum()
     return dist
 
 
-# Predict the probability tag for this example.
+# Predict the tag for this example.
 def predict(example_to_predict):
     # Create the example + bias vector.
     example_vector = np.array([1, example_to_predict[0]])
@@ -45,6 +44,14 @@ def vector_create(number):
     return vector
 
 
+# Calculate the 'real' probability.
+def true_prob(number_to_prob):
+    prob1 = norm(2, 1).pdf(number_to_prob)
+    prob2 = norm(4, 1).pdf(number_to_prob)
+    prob3 = norm(6, 1).pdf(number_to_prob)
+    return prob1 / (prob1 + prob2 + prob3)
+
+
 # Create the distributions.
 normalDistribution1 = create_normal_distribution(2, 1)
 normalDistribution2 = create_normal_distribution(4, 1)
@@ -60,50 +67,55 @@ np.random.shuffle(data)
 
 # Create the weight matrix.
 weightMatrix = np.random.random([3, 2])
-learningRate = 0.1
+# Set learning rate
+learningRate = 0.05
 
 # The practice.
-for i in range(0, 100): # TODO - CHANGE TO 100?
+for i in range(0, 100):
     for example in data:
         # Predict.
         prediction = predict(example)
-        #print 'AFTER PREDICT: weightMatrix =\n', weightMatrix
+
         # Loss calculation.
         subtractionVector = np.array(vector_create(example[1]))
         subtractionVector.shape = (3, 1)
-        #print 'prediction - subtractionVector = ', prediction, ' - ', subtractionVector
         update = prediction - subtractionVector
-        #print 'update = ', update
+
         # Update.
         exampleVector = np.array([1, example[0]])
         exampleVector.shape = (1, 2)
-        #print 'exampleVector = ', exampleVector
         updateMatrix = np.matmul(update, exampleVector)
-        #print 'BEFORE LR: updateMatrix =\n', updateMatrix
-        #print 'AFTER LR: updateMatrix =\n', learningRate * updateMatrix
         weightMatrix = weightMatrix - (learningRate * updateMatrix)
-        #print 'AFTER UPDATE: weightMatrix =\n', weightMatrix
 
 # The test.
-
 predictionSet = []
 trueProb = []
 numberList = []
-#numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-for number1 in range(0, 100):
-#for number1 in numberList:
+for number1 in range(0, 300):
     number2 = np.random.uniform(0, 10)
     prediction = predict([number2, 0])
-    if number1 % 10 == 0:
-        print 'number: ', number2, ', prediction: ', prediction[0]
     predictionSet.extend(prediction[0])
     numberList.append(number2)
-    #trueProb.append(norm.pdf(number1, 2, 1))
+    trueProb.append(true_prob(number2))
 
-#plt.plot(trueProb)
-print numberList
-print "predictionSet", predictionSet
-plt.plot(numberList, predictionSet, 'ro')
+# Draw the graph.
+fig, ax = plt.subplots()
+plt.plot(numberList, predictionSet, 'ro', label='Net prediction')
+plt.plot(numberList, trueProb, 'bo', label='True probability')
+
+# Set the legend
+legend = ax.legend(loc='upper right', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+for label in legend.get_texts():
+    label.set_fontsize('large')  # the legend text size
+for label in legend.get_lines():
+    label.set_linewidth(1.5)  # the legend line width
+
+# Set the graph.
 plt.axis([0, 10, 0, 1])
-plt.ylabel('some numbers')
+plt.ylabel('p(y = 1 | x)')
+plt.xlabel('x value')
+
+# Show the graph.
 plt.show()
